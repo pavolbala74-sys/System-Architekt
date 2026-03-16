7ed7a9bf7871747051a28fe45d7277377922ad58
pavolbala74-sys <pavolbala74@gmail.com>
2026-03-16 10:19:16 +0100

    Update jest.config.tsSS

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({ dir: './' })
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  setupFilesAfterFramework: ['<rootDir>/tests/setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
